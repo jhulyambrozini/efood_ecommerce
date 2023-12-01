2085d2419f1077b9e673fc456667c3156fc99760
jhuly ambrozini <jhulyamfebr@gmial.com>
2023-11-30 21:33:11 -0300

    test: testando componente Category

---
+++ src/components/view/Category/__tests__/category.test.jsx
@@ -0,0 +1,41 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import Category from '..'
+
+const props = {
+    image: 'img/img1.png',
+    title: 'restaurante',
+    type: 'italiana',
+    description: 'descrição...',
+    detach: true,
+    evaluation: 4,
+    id: 1
+}
+
+describe('<Category />', () => {
+    it('should render with detach correctly', () => {
+        render(<MemoryRouter>
+            <Category {...props} />
+        </MemoryRouter>)
+
+        const tagDetach = screen.getByText(/Destaque da semana/i)
+        
+        expect(tagDetach).toBeInTheDocument()
+    })
+
+    it('should call a function when clicking "Saiba mais"', () => {
+        const handleClick = jest.fn()
+        render(<MemoryRouter>
+            <Category {...props} />
+        </MemoryRouter>)
+
+        const knowMoreButton = screen.getByRole('button', { name: /saiba mais/i })
+        
+        expect(knowMoreButton).toBeInTheDocument()
+
+        knowMoreButton.onclick = handleClick
+
+        fireEvent.click(knowMoreButton)
+        expect(handleClick).toHaveBeenCalled()
+    })
+})
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,27 +8,25 @@ import { Logo } from '../../styles'
 
 import logoImage from '../../assets/images/logo.svg'
 
-const Home = () => {
-  return (
-    <>
-      <HeaderContainer>
-        <h1>
-          <Link to="/" title="Home">
-            <Logo src={logoImage} alt="EFOOD" />
-          </Link>
-        </h1>
-        <h2>
-          Viva experiências gastronômicas
-          <br />
-          no conforto da sua casa
-        </h2>
-      </HeaderContainer>
-      <main>
-        <CategorytList />
-      </main>
-      <Footer />
-    </>
-  )
-}
+const Home = () => (
+  <>
+    <HeaderContainer>
+      <h1>
+        <Link to="/" title="Home">
+          <Logo src={logoImage} alt="EFOOD" />
+        </Link>
+      </h1>
+      <h2>
+        Viva experiências gastronômicas
+        <br />
+        no conforto da sua casa
+      </h2>
+    </HeaderContainer>
+    <main>
+      <CategorytList />
+    </main>
+    <Footer />
+  </>
+)
 
 export default Home
